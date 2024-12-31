d77871e7e587562b5e8ee6b9a8a5811ba07fd297
Yassine Hachguer <yassine.hachguer05@gmail.com>
2024-12-31 20:09:33 +0100

    Implémantation de la connexion MongoDB et Redis et Export des clients et fonctions utiles

--- src/config/db.js
+++ src/config/db.js
@@ -12,14 +12,41 @@ let mongoClient, redisClient, db;
 async function connectMongo() {
   // TODO: Implémenter la connexion MongoDB
   // Gérer les erreurs et les retries
+  try {
+    mongoClient = new MongoClient(config.mongodb.uri);
+    await mongoClient.connect();
+    db = mongoClient.db(config.mongodb.dbName);
+    console.log(`Connected to MongoDB database: ${config.mongodb.dbName}`);
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  }
 }
 
 async function connectRedis() {
   // TODO: Implémenter la connexion Redis
   // Gérer les erreurs et les retries
+  try {
+    redisClient = redis.createClient({ url: config.redis.uri });
+    redisClient.on('error', (err) => console.error('Redis Client Error', err));
+    await redisClient.connect();
+    console.log('Connected to Redis');
+  } catch (err) {
+    console.error('Error connecting to Redis:', err);
+    process.exit(1);
+  }
+}
+async function closeConnections() {
+  if (mongoClient) await mongoClient.close();
+  if (redisClient) await redisClient.disconnect();
 }
 
 // Export des fonctions et clients
 module.exports = {
   // TODO: Exporter les clients et fonctions utiles
+  connectMongo,
+  connectRedis,
+  closeConnections,
+  getDb: () => db,
+  getRedisClient: () => redisClient,
 };
